4ee0e1aed01e1949a690ccf5e3048df49b2bb776
Rafael Maciel <rafael.macielf@gmail.com>
2022-06-07 16:28:44 -0300

    product title refactored

--- client/topzera/src/containers/ProductPanel/components/ProductInfo/style.js
+++ client/topzera/src/containers/ProductPanel/components/ProductInfo/style.js
@@ -3,6 +3,8 @@ import { Typography } from '../../../../components'
 
 export const TitleStyled = styled(Typography)`
     font-size: 1.4rem !important;
+    text-align: center;
+    font-weight: bold !important;
 `
 
 export const PriceStyled = styled(Typography)`
